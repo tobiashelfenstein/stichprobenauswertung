eeff7242b07508d0e2cb3851d771e5e58bff4b53
Tobias Helfenstein <tobias@die-softwarezimmerei.de>
2022-01-13 23:19:15 +0100

    Bluetooth Importer implementiert

---
+++ Stichprobenauswertung_C++/HaglofBluetoothImporter.cpp
@@ -0,0 +1,66 @@
+#include "HaglofFileImporter.h"
+
+#import <msxml3.dll>
+
+
+
+HaglofFileImporter::HaglofFileImporter()
+{
+	// nothing to do
+}
+
+HaglofFileImporter::~HaglofFileImporter()
+{
+	// nothing to do
+}
+
+bool HaglofFileImporter::importXMLFile(char *filename)
+{
+	CoInitializeEx(NULL, COINIT_SPEED_OVER_MEMORY);
+
+	this->pXMLDoc.CreateInstance(__uuidof(DOMDocument));
+	this->pXMLDoc->async = VARIANT_FALSE;
+	this->pXMLDoc->validateOnParse = VARIANT_TRUE;
+
+	this->pXMLDoc->load(filename);
+	this->pXMLDoc->setProperty("SelectionLanguage", "XPath");
+
+	return TRUE;
+}
+
+bool HaglofFileImporter::readAll()
+{
+	IXMLDOMNodeListPtr pTrees = nullptr;
+	IXMLDOMNodePtr pTree = nullptr;
+
+	pTrees = this->pXMLDoc->selectNodes("/MDIIDATA/Data/Tree");
+
+	long numTrees = 0;
+	pTrees->get_length(&numTrees);
+	for (long i = 0; i < numTrees; i++)
+	{
+		HaglofData entry;
+		pTrees->get_item(i, &pTree);
+
+		// ist wtol sicher und gut?!
+		entry.species = this->readMeasuredData(pTree, bstr_t("SpeciesText"));
+		entry.diameter = _wtol(this->readMeasuredData(pTree, bstr_t("Diameter")));
+		entry.height1 = _wtol(this->readMeasuredData(pTree, bstr_t("Height1")));
+
+		this->measuredData.push_back(entry);
+	}
+
+	return TRUE;
+}
+
+// TODO: Verbesserung Pointer to Pointer = **node; Speicherverwaltung
+BSTR HaglofFileImporter::readMeasuredData(IXMLDOMNode *node, bstr_t queryString)
+{
+	IXMLDOMNodePtr pData = nullptr;
+	BSTR value;
+
+	node->selectSingleNode(queryString, &pData);
+	pData->get_text(&value);
+
+	return value;
+}
---
+++ Stichprobenauswertung_C++/HaglofBluetoothImporter.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+
+#import <msxml3.dll>
+
+struct HaglofData
+{
+	std::wstring species;
+	long diameter;
+	long height1;
+};
+
+
+class HaglofFileImporter
+{
+private:
+	MSXML2::IXMLDOMDocument2Ptr pXMLDoc;
+	std::vector<HaglofData> measuredData;
+
+	BSTR readMeasuredData(IXMLDOMNode *node, bstr_t queryString);
+
+public:
+	HaglofFileImporter();
+	~HaglofFileImporter();
+
+	bool importXMLFile(char *filename);
+	bool readAll();
+};
+
